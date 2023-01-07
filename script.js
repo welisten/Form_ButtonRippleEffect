cf5a752eb98f756531f3934072fd4a267aacb227
Wesley Welisten <welistenw@gmail.com>
2023-01-07 19:03:18 -0300

    adicionado estilo escuro e Ripple effect no button

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const form = document.querySelector("form"),
         nextBtn = form.querySelector(".nextBtn"),
         backBtn = form.querySelector(".backBtn"),
+        submBtn = form.querySelector(".submit"),
         allInput = form.querySelectorAll(".first input");
+        
+
+
 
 nextBtn.addEventListener("click", ()=> {
     allInput.forEach(input => {
@@ -15,3 +19,50 @@ nextBtn.addEventListener("click", ()=> {
 
 backBtn.addEventListener("click", () => form.classList.remove('secActive'))
 
+// ------------------- Button Ripple Effect ---------------------------------
+
+let button
+let coordenadas
+
+
+nextBtn.addEventListener("mouseover", (e) => {
+    e.preventDefault()
+    
+    button = nextBtn
+    coordenadas = button.getBoundingClientRect();
+
+    const x = (e.pageX - coordenadas.x)
+    const y = (e.pageY - coordenadas.y)
+
+    nextBtn.style.setProperty("--xPos", x + "px")
+    nextBtn.style.setProperty("--yPos", y + "px")
+
+})
+
+backBtn.addEventListener("mouseover", (e) => {
+    e.preventDefault()
+    
+    button = backBtn
+    coordenadas = button.getBoundingClientRect();
+
+    const x = (e.pageX - coordenadas.x)
+    const y = (e.pageY - coordenadas.y)
+
+    backBtn.style.setProperty("--xPos", x + "px")
+    backBtn.style.setProperty("--yPos", y + "px")
+
+})
+
+submBtn.addEventListener("mouseover", (e) => {
+    e.preventDefault()
+    
+    button = submBtn
+    coordenadas = button.getBoundingClientRect();
+
+    const x = (e.pageX - coordenadas.x)
+    const y = (e.pageY - coordenadas.y)
+
+    submBtn.style.setProperty("--xPos", x + "px")
+    submBtn.style.setProperty("--yPos", y + "px")
+
+})
